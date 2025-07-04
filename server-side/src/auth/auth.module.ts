481f157ebfec29a6b71d137a1e8fc67331171354
iknowozey <iknowozey@gmail.com>
2025-07-05 01:33:46 +0700

    server(feat): describes basic methods of registration/authorization for a user in auth.service, describes two controllers login/register and connection in auth.module

--- server-side/src/auth/auth.module.ts
+++ server-side/src/auth/auth.module.ts
@@ -1,9 +1,36 @@
+import { JwtStrategy } from './strategies/jwt.strategy';
 import { Module } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
+import { UserModule } from 'src/user/user.module';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { JwtModule } from '@nestjs/jwt';
+import { getJwtConfig } from 'src/config/jwt.config';
+import { PrismaService } from 'src/prisma.service';
+import { UserService } from 'src/user/user.service';
+import { GoogleStrategy } from './strategies/google.strategy';
+import { GithubStrategy } from './strategies/github.strategy';
+import { YandexStrategy } from './strategies/yandex.strategy';
 
 @Module({
+  imports: [
+    UserModule,
+    ConfigModule,
+    JwtModule.registerAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: getJwtConfig,
+    }),
+  ],
   controllers: [AuthController],
-  providers: [AuthService],
+  providers: [
+    AuthService,
+    PrismaService,
+    UserService,
+    JwtStrategy,
+    GoogleStrategy,
+    GithubStrategy,
+    YandexStrategy,
+  ],
 })
 export class AuthModule {}
--- server-side/src/auth/auth.service.ts
+++ server-side/src/auth/auth.service.ts
@@ -1,4 +1,64 @@
-import { Injectable } from '@nestjs/common';
+import {
+  BadRequestException,
+  Injectable,
+  NotFoundException,
+} from '@nestjs/common';
+import { JwtService } from '@nestjs/jwt';
+import { PrismaService } from 'src/prisma.service';
+import { UserService } from 'src/user/user.service';
+import { AuthDto } from './dto/auth.dto';
+
+interface AuthTokens {
+  accessToken: string;
+  refreshToken: string;
+}
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+  constructor(
+    private jwt: JwtService,
+    private userService: UserService,
+    private prisma: PrismaService,
+  ) {}
+
+  issueTokens(userId: string): AuthTokens {
+    const data = { id: userId };
+
+    const accessToken = this.jwt.sign(data, {
+      expiresIn: '30m',
+    });
+
+    const refreshToken = this.jwt.sign(data, {
+      expiresIn: '14d',
+    });
+
+    return { accessToken, refreshToken };
+  }
+
+  private async validateUser(dto: AuthDto) {
+    const user = await this.userService.getByEmail(dto.email);
+
+    if (!user) throw new NotFoundException('Пользователь не найден');
+
+    return user;
+  }
+
+  async register(dto: AuthDto) {
+    const userExist = await this.userService.getByEmail(dto.email);
+
+    if (userExist)
+      throw new BadRequestException('Пользователь уже зарегистрирован');
+
+    const user = await this.userService.create(dto);
+    const tokens = this.issueTokens(user.id);
+
+    return { user, ...tokens };
+  }
+
+  async login(dto: AuthDto) {
+    const user = await this.validateUser(dto);
+    const tokens = this.issueTokens(user.id);
+
+    return { user, ...tokens };
+  }
+}
