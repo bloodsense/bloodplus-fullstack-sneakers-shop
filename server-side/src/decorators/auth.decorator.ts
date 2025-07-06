42ca4a3296de80594381b2470d1c76e49828dc5c
iknowozey <iknowozey@gmail.com>
2025-07-07 06:53:46 +0700

    server(feat): added guard and decorator for admin panel

--- server-side/src/decorators/auth.decorator.ts
+++ server-side/src/decorators/auth.decorator.ts
@@ -1,4 +1,9 @@
-import { UseGuards } from '@nestjs/common';
+import { applyDecorators, UseGuards } from '@nestjs/common';
+import { UserRole } from 'generated/prisma';
 import { JwtAuthGuard } from 'src/guards/jwt.auth.guard';
+import { RolesGuard } from 'src/guards/roles.guard';
+import { Roles } from './roles.decorator';
 
-export const Auth = () => UseGuards(JwtAuthGuard);
+export function Auth(...roles: UserRole[]) {
+  return applyDecorators(UseGuards(JwtAuthGuard, RolesGuard), Roles(...roles));
+}
