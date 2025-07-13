21531e0b8de0ec6fc9e33bfe76da511ac6a6fe19
bloodsense <iknowozey@gmail.com>
2025-07-13 17:24:54 +0700

    client: added variables for SEO, updated main layout

---
+++ client-side/constants/seo-constants.ts
@@ -0,0 +1,5 @@
+export const NO_INDEX_PAGE = { robots: { index: false, follow: false } }
+
+export const SITE_NAME = 'bloodplus'
+
+export const SITE_DESCRIPTION = 'Мы продаем не обувь, а чистую энергию улиц'
