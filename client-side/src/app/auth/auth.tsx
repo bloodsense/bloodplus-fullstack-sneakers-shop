ac0cee6d31ad9256135465dd4ea5ab268af57199
bloodsense <iknowozey@gmail.com>
2025-07-17 23:47:31 +0700

    client(ui): fixed bug with displaying logo on light theme

--- client-side/src/app/auth/auth.tsx
+++ client-side/src/app/auth/auth.tsx
@@ -28,9 +28,17 @@ export const Auth: React.FC = ({}) => {
 					alt="bloodplus-logo-light"
 					width={500}
 					height={500}
+					className="dark:hidden"
+				/>
+				<Image
+					src="/bloodplus-logo-light.svg"
+					alt="bloodplus-logo-dark"
+					width={500}
+					height={500}
+					className="hidden dark:block"
 				/>
 			</div>
-			<div className="h-full flex flex-col items-center justify-center bg-foreground/10">
+			<div className="h-full flex flex-col items-center justify-center bg-foreground/20">
 				<Card className="border-none p-10">
 					<CardHeader className="text-center w-full">
 						<CardTitle className="text-3xl font-bold">
