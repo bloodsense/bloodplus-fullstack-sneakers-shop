db7c380c0a8c78a72bf710fc046511eb6f668a3d
bloodsense <iknowozey@gmail.com>
2025-07-17 22:46:52 +0700

    client(feat): added support for dark/light theme

--- client-side/src/app/layout.tsx
+++ client-side/src/app/layout.tsx
@@ -2,8 +2,9 @@ import type { Metadata } from 'next'
 import { Montserrat } from 'next/font/google'
 import './globals.css'
 import { SITE_DESCRIPTION, SITE_NAME } from '../constants/seo-constants'
-import { Providers } from './providers'
+import { Providers } from '../providers/query-client-provider'
 import { Toaster } from 'sonner'
+import { ThemeProvider } from '@/providers/theme-provider'
 
 const montserrat = Montserrat({
 	variable: '--font-geist-sans',
@@ -23,9 +24,18 @@ export default function RootLayout({
 	children: React.ReactNode
 }>) {
 	return (
-		<html lang="ru">
+		<html lang="ru" suppressHydrationWarning>
 			<body className={`${montserrat.variable} antialiased`}>
-				<Providers>{children}</Providers>
+				<Providers>
+					<ThemeProvider
+						attribute="class"
+						defaultTheme="system"
+						enableSystem
+						disableTransitionOnChange
+					>
+						{children}
+					</ThemeProvider>
+				</Providers>
 				<Toaster />
 			</body>
 		</html>
